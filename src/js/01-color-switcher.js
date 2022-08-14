cb640531467cbc3b3231f01b2ddbd987c1ec482b
Serhii Bohosta <bigbowse@Serhiis-MacBook-Pro.local>
2022-08-15 00:28:15 +0300

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let timer = null;
+
+buttonStop.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function bgColorSwitch() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onButtonStart() {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+
+  timer = setInterval(bgColorSwitch, 1000);
+}
+
+function onButtonStop() {
+  clearInterval(timer);
+
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+}
+
+buttonStart.addEventListener('click', onButtonStart);
+buttonStop.addEventListener('click', onButtonStop);
 
