bbabab320eaad71ee5d32ba6581ce4b5595a4026
Serhii Bohosta <bigbowse@Serhiis-MacBook-Pro.local>
2022-08-16 23:20:32 +0300

    done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });  
+}
+
+function onButtonClick(event) {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let delayEl = Number(delay.value);
+  const stepEl = Number(step.value);
+  const amounEl = Number(amount.value);
+
+  for (let i = 1; i <= amounEl; i += 1) {
+    createPromise(i, delayEl)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayEl += stepEl;
   }
 }
+
+formEl.addEventListener('submit', onButtonClick);
+
+
